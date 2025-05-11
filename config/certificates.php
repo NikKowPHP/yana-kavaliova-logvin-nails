2c1f609945738ea4c4f53eac9fef09cfb789bbb6
NikKowPHP <nik.kow@outlook.com>
2025-05-11 17:04:11 +0200

    Update certificate configuration and add new data file for course completion records

--- config/certificates.php
+++ config/certificates.php
@@ -2,14 +2,32 @@
 return [
     'certificates' => [
         [
-            'id' => 'certyfikat-manicure-hybrydowy-2023',
-            'title_pl' => 'Karta Licencyjna',
+            'id' => 'atraumatyczny-manicure-2025',
+            'title_pl' => '«ATRAUMATYCZNY MANICURE»',
             'description_pl' => 'Certyfikat ukończenia kursu.',
-            'original_file_path' => 'assets/certificates/originals/manicure_scan_6.png',
+            'original_file_path' => 'assets/certificates/originals/manicure_scan_1.jpg',
             'original_file_type' => 'image',
-            'thumbnail_image_path' => 'assets/certificates/thumbnails/manicure_scan_6.jpg',
-            'translation_pl' => 'IMIĘ I NAZWISKO: Kowalowa-Łogwin Jana Nikołajewna
-Numer licencji: BY - 17057 - 0224-81-7
+            'thumbnail_image_path' => 'assets/certificates/thumbnails/manicure_scan_1_thumb.jpg',
+            'translation_pl' => 'Certyfikat
+potwierdza, że
+Kovalowa-Łogwin Jana Nikołajewna
+pomyślnie ukończyła program teoretyczny i praktyczny kursu
+«ATRAUMATYCZNY MANICURE»
+bez prawa nauczania
+15-16.01.2025
+Wykładowca Kirsanowa S.W.
+',
+        ],
+        [
+            'id' => 'karta-licencyjna-smart-pedicure-2024',
+            'title_pl' => 'Karta Licencyjna',
+            'description_pl' => 'Karta Licencyjna Smart Pedicure.',
+            'original_file_path' => 'assets/certificates/originals/manicure_scan_6.jpg',
+            'original_file_type' => 'image',
+            'thumbnail_image_path' => 'assets/certificates/thumbnails/manicure_scan_6_thumb.jpg',
+            'translation_pl' => 'Karta Licencyjna
+IMIĘ I NAZWISKO: Kowalowa-Łogwin Jana Nikołajewna
+Numer licencji: WU - 17057 - 0224-81-7
 Niniejsza karta licencyjna jest dokumentem potwierdzającym, że licencjobiorca legalnie uzyskał prawo do korzystania z opatentowanej techniki "SMART pedicure":
 w praktyce prywatnej (bez prawa nauczania i odsprzedaży)
 w technice pedicure dyskami Smart
@@ -21,67 +39,74 @@ Data: 27.02.2024
 ',
         ],
         [
-            'id' => 'certyfikat-pedicure-klasyczny-2024',
-            'title_pl' => '«ATRAUMATYCZNY MANICURE»',
+            'id' => 'smart-pedicure-w-40-min-2014',
+            'title_pl' => 'SMART pedicure w 40 min',
             'description_pl' => 'Certyfikat ukończenia kursu.',
-            'original_file_path' => 'assets/certificates/originals/manicure_scan_1.jpg',
-            'original_file_type' => 'image',
-            'thumbnail_image_path' => 'assets/certificates/thumbnails/pedicure_classic_2024_thumb.jpg',
-            'translation_pl' => 'Certyfikat
-potwierdza, że
-Kovalowa-Łogwin Jana Nikołajewna
-pomyślnie ukończyła program teoretyczny i praktyczny kursu
-«ATRAUMATYCZNY MANICURE»
-bez prawa nauczania
-15-16.01.2025 
-Wykładowca Kirsanowa S.W.
-',
-        ],
-        // Add more certificates
-        [
-            'id' => 'manicure-scan-1',
-            'title_pl' => 'Skan Manicure 1',
-            'description_pl' => 'Skan dokumentu dotyczącego manicure.',
-            'original_file_path' => 'assets/certificates/originals/manicure_scan_2.jpg',
-            'original_file_type' => 'image',
-            'thumbnail_image_path' => 'assets/certificates/thumbnails/manicure_scan_1_thumb.jpg',
-            'translation_pl' => 'Brak dostępnego tłumaczenia.',
-        ],
-        [
-            'id' => 'manicure-scan-2',
-            'title_pl' => 'Skan Manicure 2',
-            'description_pl' => 'Skan dokumentu dotyczącego manicure.',
             'original_file_path' => 'assets/certificates/originals/manicure_scan_2.jpg',
             'original_file_type' => 'image',
             'thumbnail_image_path' => 'assets/certificates/thumbnails/manicure_scan_2_thumb.jpg',
-            'translation_pl' => 'Brak dostępnego tłumaczenia.',
+            'translation_pl' => 'Certyfikat
+o ukończeniu kursu autorskiego
+Kowaliowa-Łogwin [Imię - Handwritten, likely Yana]
+pomyślnie ukończyła program
+SMART pedicure
+w 40 min
+z nadaniem statusu
+SMART MASTER
+24-28.08.2014, [Lokalizacja - Handwritten, difficult to read clearly]
+www.smart-pilka.ru
+',
         ],
         [
-            'id' => 'manicure-scan-3',
-            'title_pl' => 'Skan Manicure 3',
-            'description_pl' => 'Skan dokumentu dotyczącego manicure.',
+            'id' => 'estetyka-w-pedicure-2014',
+            'title_pl' => 'ESTETYKA W PEDICURE',
+            'description_pl' => 'Certyfikat ukończenia kursu.',
             'original_file_path' => 'assets/certificates/originals/manicure_scan_3.jpg',
             'original_file_type' => 'image',
             'thumbnail_image_path' => 'assets/certificates/thumbnails/manicure_scan_3_thumb.jpg',
-            'translation_pl' => 'Brak dostępnego tłumaczenia.',
+            'translation_pl' => 'Certyfikat
+BEZ PRAWA NAUCZANIA
+KOMU: Kowaliowa - Łogwin Yana
+O UKOŃCZENIU KURSU AUTORSKIEGO "ESTETYKA W PEDICURE"
+DATA: 24-28.02.2014
+podpis
+KIRSA
+NOVA
+',
         ],
         [
-            'id' => 'manicure-scan-4',
-            'title_pl' => 'Skan Manicure 4',
-            'description_pl' => 'Skan dokumentu dotyczącego manicure.',
+            'id' => 'aparatowy-manicure-wzmacnianie-zelem-2013',
+            'title_pl' => 'APARATOWY MANICURE + WZMACNIANIE ŻELEM BEZ OPILOWANIA',
+            'description_pl' => 'Certyfikat ukończenia kursu.',
             'original_file_path' => 'assets/certificates/originals/manicure_scan_4.jpg',
             'original_file_type' => 'image',
             'thumbnail_image_path' => 'assets/certificates/thumbnails/manicure_scan_4_thumb.jpg',
-            'translation_pl' => 'Brak dostępnego tłumaczenia.',
+            'translation_pl' => 'Certyfikat
+BEZ PRAWA NAUCZANIA
+KOMU: Kowaliowa - Łogwin [Imię - Handwritten, likely Yana]
+O UKOŃCZENIU KURSU AUTORSKIEGO "APARATOWY MANICURE + WZMACNIANIE ŻELEM BEZ OPILOWANIA"
+DATA: 26-27.10.2013
+podpis
+KIRSA
+NOVA
+',
         ],
         [
-            'id' => 'manicure-scan-5',
-            'title_pl' => 'Skan Manicure 5',
-            'description_pl' => 'Skan dokumentu dotyczącego manicure.',
+            'id' => 'kombinowany-manicure-2015',
+            'title_pl' => 'KOMBINOWANY MANICURE',
+            'description_pl' => 'Certyfikat ukończenia kursu.',
             'original_file_path' => 'assets/certificates/originals/manicure_scan_5.jpg',
             'original_file_type' => 'image',
             'thumbnail_image_path' => 'assets/certificates/thumbnails/manicure_scan_5_thumb.jpg',
-            'translation_pl' => 'Brak dostępnego tłumaczenia.',
+            'translation_pl' => 'Certyfikat
+BEZ PRAWA NAUCZANIA
+KOMU: Kowaliowa - Łogwin [Imię - Handwritten, likely Yana]
+O UKOŃCZENIU AUTORSKIEGO KURSU PODSTAWOWEGO «KOMBINOWANY MANICURE».
+DATA: 13-14.09.2015
+podpis
+KIRSA
+NOVA
+',
         ],
     ]
 ];
---
+++ data.md
@@ -0,0 +1,75 @@
+Certyfikat
+potwierdza, że
+Kovalowa-Łogwin Jana Nikołajewna
+pomyślnie ukończyła program teoretyczny i praktyczny kursu
+«ATRAUMATYCZNY MANICURE»
+bez prawa nauczania
+15-16.01.2025 
+Wykładowca Kirsanowa S.W.
+ 
+ photo - manicure_scan_1
+
+ Karta Licencyjna
+IMIĘ I NAZWISKO: Kowalowa-Łogwin Jana Nikołajewna
+Numer licencji: WU - 17057 - 0224-81-7
+Niniejsza karta licencyjna jest dokumentem potwierdzającym, że licencjobiorca legalnie uzyskał prawo do korzystania z opatentowanej techniki "SMART pedicure":
+w praktyce prywatnej (bez prawa nauczania i odsprzedaży)
+w technice pedicure dyskami Smart
+Niniejsza metoda jest chroniona przepisami prawa Rosji oraz umowami międzynarodowymi w zakresie ochrony praw autorskich.
+Numer licencji wskazany na karcie licencyjnej jest przypisany licencjobiorcy w celu jego identyfikacji jako legalnego użytkownika opatentowanej techniki "Smart pedicure".
+Licencjonowany wykładowca: Kirsanowa S.W.
+Licencjodawca: SMART LTD
+Data: 27.02.2024
+
+ photo - manicure_scan_6
+
+
+ Certyfikat
+o ukończeniu kursu autorskiego
+Kowaliowa-Łogwin [Imię - Handwritten, likely Yana]
+pomyślnie ukończyła program
+SMART pedicure
+w 40 min
+z nadaniem statusu
+SMART MASTER
+24-28.08.2014, [Lokalizacja - Handwritten, difficult to read clearly]
+www.smart-pilka.ru
+
+ photo - manicure_scan_2
+
+
+Certyfikat
+BEZ PRAWA NAUCZANIA
+KOMU: Kowaliowa - Łogwin Yana
+O UKOŃCZENIU KURSU AUTORSKIEGO "ESTETYKA W PEDICURE"
+DATA: 24-28.02.2014
+podpis
+KIRSA
+NOVA
+
+ photo - manicure_scan_3
+
+
+
+Certyfikat
+BEZ PRAWA NAUCZANIA
+KOMU: Kowaliowa - Łogwin [Imię - Handwritten, likely Yana]
+O UKOŃCZENIU KURSU AUTORSKIEGO "APARATOWY MANICURE + WZMACNIANIE ŻELEM BEZ OPILOWANIA"
+DATA: 26-27.10.2013
+podpis
+KIRSA
+NOVA
+
+ photo - manicure_scan_4
+
+
+Certyfikat
+BEZ PRAWA NAUCZANIA
+KOMU: Kowaliowa - Łogwin [Imię - Handwritten, likely Yana]
+O UKOŃCZENIU AUTORSKIEGO KURSU PODSTAWOWEGO «KOMBINOWANY MANICURE».
+DATA: 13-14.09.2015
+podpis
+KIRSA
+NOVA
+
+ photo - manicure_scan_5
