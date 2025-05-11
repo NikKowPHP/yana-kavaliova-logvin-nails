681a06d189a087005759bbba80ae5cca440302ae
NikKowPHP <nik.kow@outlook.com>
2025-05-11 16:51:27 +0200

    Refactor code structure for improved readability and maintainability

--- config/certificates.php
+++ config/certificates.php
@@ -3,28 +3,46 @@ return [
     'certificates' => [
         [
             'id' => 'certyfikat-manicure-hybrydowy-2023',
-            'title_pl' => 'Certyfikat Manicure Hybrydowy 2023',
-            'description_pl' => 'Zaawansowany kurs manicure hybrydowego.',
-            'original_file_path' => 'assets/certificates/originals/manicure_hybrid_2023_orig.png',
+            'title_pl' => 'Karta Licencyjna',
+            'description_pl' => 'Certyfikat ukończenia kursu.',
+            'original_file_path' => 'assets/certificates/originals/manicure_scan_6.png',
             'original_file_type' => 'image',
-            'thumbnail_image_path' => 'assets/certificates/thumbnails/manicure_hybrid_2023_thumb.jpg',
-            'translation_pl' => 'To jest przykładowe tłumaczenie tekstu dla Certyfikatu Manicure Hybrydowy 2023.',
+            'thumbnail_image_path' => 'assets/certificates/thumbnails/manicure_scan_6.jpg',
+            'translation_pl' => 'IMIĘ I NAZWISKO: Kowalowa-Łogwin Jana Nikołajewna
+Numer licencji: BY - 17057 - 0224-81-7
+Niniejsza karta licencyjna jest dokumentem potwierdzającym, że licencjobiorca legalnie uzyskał prawo do korzystania z opatentowanej techniki "SMART pedicure":
+w praktyce prywatnej (bez prawa nauczania i odsprzedaży)
+w technice pedicure dyskami Smart
+Niniejsza metoda jest chroniona przepisami prawa Rosji oraz umowami międzynarodowymi w zakresie ochrony praw autorskich.
+Numer licencji wskazany na karcie licencyjnej jest przypisany licencjobiorcy w celu jego identyfikacji jako legalnego użytkownika opatentowanej techniki "Smart pedicure".
+Licencjonowany wykładowca: Kirsanowa S.W.
+Licencjodawca: SMART LTD
+Data: 27.02.2024
+',
         ],
         [
             'id' => 'certyfikat-pedicure-klasyczny-2024',
-            'title_pl' => 'Certyfikat Pedicure Klasyczny 2024',
-            'description_pl' => 'Podstawowy kurs pedicure klasycznego.',
-            'original_file_path' => 'assets/certificates/originals/pedicure_classic_2024_orig.png',
+            'title_pl' => '«ATRAUMATYCZNY MANICURE»',
+            'description_pl' => 'Certyfikat ukończenia kursu.',
+            'original_file_path' => 'assets/certificates/originals/manicure_scan_1.jpg',
             'original_file_type' => 'image',
             'thumbnail_image_path' => 'assets/certificates/thumbnails/pedicure_classic_2024_thumb.jpg',
-            'translation_pl' => 'To jest przykładowe tłumaczenie tekstu dla Certyfikatu Pedicure Klasyczny 2024.',
+            'translation_pl' => 'Certyfikat
+potwierdza, że
+Kovalowa-Łogwin Jana Nikołajewna
+pomyślnie ukończyła program teoretyczny i praktyczny kursu
+«ATRAUMATYCZNY MANICURE»
+bez prawa nauczania
+15-16.01.2025 
+Wykładowca Kirsanowa S.W.
+',
         ],
         // Add more certificates
         [
             'id' => 'manicure-scan-1',
             'title_pl' => 'Skan Manicure 1',
             'description_pl' => 'Skan dokumentu dotyczącego manicure.',
-            'original_file_path' => 'assets/certificates/originals/manicure_scan_1.jpg',
+            'original_file_path' => 'assets/certificates/originals/manicure_scan_2.jpg',
             'original_file_type' => 'image',
             'thumbnail_image_path' => 'assets/certificates/thumbnails/manicure_scan_1_thumb.jpg',
             'translation_pl' => 'Brak dostępnego tłumaczenia.',
